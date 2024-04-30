2eba335662c19e1f20df690664c192fe7b279c54
Gustian <125520997+gustianmajs@users.noreply.github.com>
2024-04-30 21:34:33 +0700

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,5 @@
+Project 4 | Digital Skola
+
+Nama: Gustian
+Kelompok: A
+Grup: 3
---
+++ scripts/etl.py
@@ -0,0 +1,27 @@
+import mysql.connector as connection
+import pandas as pd
+import time
+from sqlalchemy import create_engine
+import psycopg2
+
+
+try:
+    mydb = connection.connect(host="localhost", database = 'operational', user="root", passwd="mysql", use_pure=True, port=3305)
+
+    query = "Select * from youtube;"
+    result_dataFrame = pd.read_sql(query, mydb)
+    print(result_dataFrame)
+
+    conn_string = 'postgresql://postgres:postgres@localhost/data_warehouse'
+
+    db = create_engine(conn_string)
+    conn = db.connect()
+
+    start_time = time.time()
+    result_dataFrame.to_sql('youtube_etl', con=conn, if_exists='replace', index=False)
+    print("to_sql duration: {} seconds".format(time.time() - start_time))
+
+    mydb.close()
+except Exception as e:
+    mydb.close()
+    print(str(e))
